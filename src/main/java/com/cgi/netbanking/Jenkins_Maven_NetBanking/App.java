87b24bb1e52fd5208e1f4b55c1fac93cb20a60bd
Sri Harsha Korlapati <sriharsha.korlapati555@gmail.com>
2017-10-26 10:20:09 +0530

    first commit

---
+++ src/main/java/com/cgi/netbanking/Jenkins_Maven_NetBanking/App.java
@@ -0,0 +1,13 @@
+package com.cgi.netbanking.Jenkins_Maven_NetBanking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
